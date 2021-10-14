c967de30e6fd871480079e61a31bc905e84abe54
Danilo <didoherculano@gmail.com>
2021-10-14 17:06:40 -0300

    layout basico adicionado

---
+++ src/components/game/Game.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './game.css'
+
+function Game(props) {
+    return ( 
+        <div className="game">
+            Game
+        </div>
+     );
+}
+
+export default Game;
--- src/components/main/App.jsx
+++ src/components/main/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Game from '../game/Game'
+
 function App() {
   return (
     <div className="App">
-      Working
+      <h1>Detetive</h1>
+      <Game/>
     </div>
   );
 }
